a3e4ff81015b9693bebcedc7aca94d60160f1843
Jack Humbert <jack.humb@gmail.com>
2020-01-19 00:06:56 -0500

    start writing c

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,8 +1,60 @@
+/* Copyright 2020 Jack Humbert
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdint.h>
-
-
-__sfr __at (0x93) CLRWDT;
+#include "sh68f83.h"
 
 void main(void) {
-	CLRWDT = 0x55;
+	B = 0xA5;
+    PRCON = 0x00;
+    CLRWDT = 0x55;
+    if (MODE_FG & POF)
+        // L0002
+    else {
+        if (!(MODE_FG & USBRST))
+            // L0003        
+        else {
+            if (&0x20 != 0x41 || &0x20 != &0x24 || &0x21 != 0x4B || 
+                &0x22 != 0x49 || &0x23 != 0x52) {
+                // L0003
+            }
+
+            // L0004
+            CLRWDT = 0x55;
+            SP = 0x27;
+            PSW = 0;
+            P4CON = 0x60;
+            P4 = 0x7F;
+            if (MODE_FG & 0xA) {
+                DFC = 0x01
+                // L0006
+                DPTR = 0x3FBA;
+                R0 = 0x20;
+                R1 = 0x06;
+                // L0579
+                while (B != 0xA5);
+                // L0580
+                
+                // L0007
+                DFC = 0xC2;
+            }
+            // L0005
+            // L0008
+            DFC = 0xCA;
+            PREWDT = 0x03;
+        }
+    }
 }
