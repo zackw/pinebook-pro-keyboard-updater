26ffd8138f691feab0e98716caf08a8f0936887c
Jack Humbert <jack.humb@gmail.com>
2020-01-30 16:22:07 -0500

    add iso version

--- firmware/src/include/keymap.h
+++ firmware/src/include/keymap.h
@@ -66,3 +66,31 @@
         NK,  NK,  NK,  NK,  NK,  NK,  NK,  NK,  \
         NK,  NK,  NK,  NK,  NK,  NK,  NK,  NK   \
     }
+
+#define KEYMAP_ISO( \
+    esc, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12,          \
+    grv,  n1, n2, n3, n4, n5, n6, n7, n8, n9, n0, min, eql, bkp,     \
+    tab,    q,  w,  e,  r,  t,  y,  u,  i,  o,  p,  lbr, rbr, ent,   \
+    cap,     a,  s,  d,  f,  g,  h,  j,  k,  l,  sem, quo, bsl,      \
+    lsf, nub, z,  x,  c,  v,  b,  n,  m, com, dot, sls, rsf, up,     \
+    lct, fn, pi, lal,         spc,           ral, rct, lef, dn, rht) \
+    { \
+        NK,  fn,  pi,  NK,  NK,  lct, NK,  NK, \
+        NK,  NK,  NK,  lsf, cap, tab, NK,  NK, \
+        NK,  NK,  NK,  NK,  NK,  NK,  NK,  NK, \
+        NK,  nub, a,   q,   NK,  grv, esc, NK, \
+        lal, z,   s,   w,   n1,  NK,  f1,  NK, \
+        NK,  x,   d,   e,   n2,  NK,  f2,  NK, \
+        NK,  c,   f,   r,   n3,  NK,  f3,  NK, \
+        NK,  v,   g,   t,   n4,  NK,  f4,  NK, \
+        NK,  b,   h,   y,   n5,  NK,  f5,  NK, \
+        NK,  n,   j,   u,   n6,  f7,  f6,  NK, \
+        NK,  m,   k,   i,   n8,  n7,  f8,  NK, \
+        NK,  com, l,   o,   n9,  f10, f9,  NK, \
+        NK,  dot, sem, p,   min, n0,  f11, NK, \
+        NK,  sls, quo, lbr, eql, NK,  f12, NK, \
+        spc, rsf, bsl, rbr, lef, ent, NK,  NK, \
+        ral, rct, up,  dn,  rht, bkp, NK,  NK, \
+        NK,  NK,  NK,  NK,  NK,  NK,  NK,  NK, \
+        NK,  NK,  NK,  NK,  NK,  NK,  NK,  NK  \
+    }
