913f97cba9b11f224e1445108333f28bb077c801
Jack Humbert <jack.humb@gmail.com>
2020-01-30 13:22:57 -0500

    cm

--- firmware/src/keymaps/jack_ansi.c
+++ firmware/src/keymaps/jack_ansi.c
@@ -328,7 +328,6 @@ ADDR(0x06FE) = 0xC9; // jump to L0435/0x06C8
 // make 0x0C release the keypad keys instead - call 0x078F / L0397 ish
 ADDR(0x07C3)[] = {  0x12, 0x07, 0x8F, // call 0x078F
                     0x12, 0x17, 0x0D, // call L0143
-                    // 0x22,             // return
                     0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 #define GET_DPL(addr) ((addr) & 0xFF)
